5ff06fbbb0c4a0e30e7a49264d45223a8637ba21
lin <alinazubair4080@gmail.com>
2024-10-24 15:33:22 +0500

    Built web
    Branch: main
    Commit: b161b584de29e79a585ffb322d404375d99c2d53 (dirty)
    
    package:peanut 5.1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3debc5a8dc60f399651a745a890aed07
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "5cb50346454291c09338a221e4998b39",
+"flutter_bootstrap.js": "7e86cdacee04db8e50c26a729bd107a4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
